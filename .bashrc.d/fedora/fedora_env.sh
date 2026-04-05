7d318ab8758837226071eb6545a6eced5722691b
sadghsbaj <colinwsnm@gmail.com>
2026-04-06 01:41:46 +0200

    chore(fedora): update flutter path and add go environment variables

--- .bashrc.d/fedora/fedora_env.sh
+++ .bashrc.d/fedora/fedora_env.sh
@@ -1,2 +1,6 @@
 export ANDROID_EMU_GPU_DRIVERS_CHECK_MESA=1
-export PATH="$HOME/development/flutter/bin:$HOME/flutter/bin:$PATH"
+
+export PATH="$HOME/.flutter/bin:$HOME/.go/bin:$PATH"
+
+export GOPATH="$HOME/.go"
+export PATH="$GOPATH/bin:$PATH"
